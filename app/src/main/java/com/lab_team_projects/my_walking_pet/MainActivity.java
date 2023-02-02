a03b7049302fedbb11016189b1184b5b754afe17
wndudwkd003 <ymail3@naver.com>
2023-02-02 20:51:06 +0900

    홈화면과 각 프래그먼트 이동

--- app/src/main/java/com/lab_team_projects/my_walking_pet/MainActivity.java
+++ app/src/main/java/com/lab_team_projects/my_walking_pet/MainActivity.java
@@ -1,24 +1,29 @@
 package com.lab_team_projects.my_walking_pet;
 
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.lab_team_projects.my_walking_pet.databinding.ActivityMainBinding;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    public ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // 뷰바인딩
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // 앱바 설정
@@ -32,5 +37,19 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
+
+        navController.addOnDestinationChangedListener((navController1, navDestination, bundle) -> {
+            if (navDestination.getId() == R.id.homeFragment) {
+                binding.appBarLayout.setVisibility(View.GONE);
+            } else {
+                binding.appBarLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        binding.ibHome.setOnClickListener(v->{
+            onBackPressed();
+        });
     }
+
 }
+
--- app/src/main/java/com/lab_team_projects/my_walking_pet/home/HomeFragment.java
+++ app/src/main/java/com/lab_team_projects/my_walking_pet/home/HomeFragment.java
@@ -1,66 +1,70 @@
 package com.lab_team_projects.my_walking_pet.home;
 
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.lab_team_projects.my_walking_pet.MainActivity;
 import com.lab_team_projects.my_walking_pet.R;
+import com.lab_team_projects.my_walking_pet.databinding.FragmentHomeBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentHomeBinding binding;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        binding.ibAR.setOnClickListener(v->{
+            Toast.makeText(requireContext(), "AR 이동 버튼", Toast.LENGTH_SHORT).show();
+        });
+
+        binding.ibShop.setOnClickListener(v->{
+            Navigation.findNavController(v).navigate(R.id.shopFragment, null);
+
+        });
+
+        binding.ibSetting.setOnClickListener(v->{
+            Navigation.findNavController(v).navigate(R.id.settingFragment, null);
+        });
+        
+        binding.ibMission.setOnClickListener(v->{
+            Navigation.findNavController(v).navigate(R.id.missionFragment, null);
+        });
+
+        binding.ibCollection.setOnClickListener(v->{
+            Navigation.findNavController(v).navigate(R.id.collectionFragment, null);
+        });
+
+        binding.ibHelp.setOnClickListener(v->{
+            Toast.makeText(requireContext(), "도움말 버튼", Toast.LENGTH_SHORT).show();
+        });
+
+
+
+        return binding.getRoot();
     }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+
+
 }
