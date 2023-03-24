41d181391da76a5860728427e529b1f4ddd09497
wndudwkd003 <ymail3@naver.com>
2023-03-24 14:57:08 +0900

    알 구매 다이얼로그

---
+++ app/src/main/java/com/lab_team_projects/my_walking_pet/shop/CustomEggDialog.java
@@ -0,0 +1,60 @@
+package com.lab_team_projects.my_walking_pet.shop;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.lab_team_projects.my_walking_pet.R;
+import com.lab_team_projects.my_walking_pet.app.GameManager;
+import com.lab_team_projects.my_walking_pet.databinding.CustomEggDialogBinding;
+import com.lab_team_projects.my_walking_pet.databinding.CustomPurchaseDialogBinding;
+import com.lab_team_projects.my_walking_pet.helpers.InventoryHelper;
+import com.lab_team_projects.my_walking_pet.home.Item;
+import com.lab_team_projects.my_walking_pet.home.ItemDetail;
+import com.lab_team_projects.my_walking_pet.login.User;
+
+import java.io.IOException;
+
+public class CustomEggDialog extends Dialog {
+
+    private CustomEggDialogBinding binding;
+    private Context context;
+
+    private ItemPurchaseListener itemPurchaseListener;    // 구매 했다고 단방향으로 알려주는 리스너 인터페이스
+
+    public void setItemPurchaseListener(ItemPurchaseListener itemPurchaseListener) {
+        this.itemPurchaseListener = itemPurchaseListener;
+    }
+
+    public interface ItemPurchaseListener {
+        void itemPurchase();
+
+    }
+
+    public CustomEggDialog(@NonNull Context context) throws IOException {
+        super(context);
+        this.context = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = CustomEggDialogBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getWindow().setBackgroundDrawableResource(R.drawable.bg_dialog_blue);
+        // 스마트폰의 화면 크기를 가져옴
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        int screenWidth = metrics.widthPixels;
+        int screenHeight = metrics.heightPixels;
+        this.getWindow().setLayout((int)(screenWidth * 0.9), (int)(screenHeight * 0.5));
+
+
+        binding.btnCancel.setOnClickListener(v->this.dismiss());    // 취소 버튼
+    }
+
+}
