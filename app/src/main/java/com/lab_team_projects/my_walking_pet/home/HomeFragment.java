465a6012d3c58a13ca6202a4209ec1779d1e8d05
wndudwkd003 <ymail3@naver.com>
2023-03-30 22:33:42 +0900

    돈증가 나중에 코드 수정해야함

--- app/src/main/java/com/lab_team_projects/my_walking_pet/home/HomeFragment.java
+++ app/src/main/java/com/lab_team_projects/my_walking_pet/home/HomeFragment.java
@@ -35,7 +35,8 @@ import java.io.IOException;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.OnSharedPreferenceChangeListener sharedPreferenceChangeListener;
     private boolean isInteractionBtnClick = false;
     long lastClickTime = 0;
     int canDragTime = 3000;    // 드래그 쿨타임 현재 3초
@@ -92,19 +93,21 @@ public class HomeFragment extends Fragment {
             binding.tvWalkCount.setText(String.valueOf(walk.getCount()));
         });
 
-        SharedPreferences sharedPreferences = requireContext().getSharedPreferences(UserPreferenceHelper.UserTokenKey.login.name(), Context.MODE_PRIVATE);
-        sharedPreferences.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
-            @Override
-            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-                if (key.equals(UserPreferenceHelper.UserPreferenceKey.money.name())) {
-                    int money = sharedPreferences.getInt(key, GameManager.getInstance().getUser().getMoney());
-                    GameManager.getInstance().getUser().setMoney(money);
-                    binding.tvMoney.setText(String.valueOf(money));
-                }
-            }
-        });
+        sharedPreferences = requireContext().getSharedPreferences(UserPreferenceHelper.UserTokenKey.login.name(), Context.MODE_PRIVATE);
 
+        sharedPreferenceChangeListener =
+                new SharedPreferences.OnSharedPreferenceChangeListener() {
+                    @Override
+                    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                        if (key.equals(UserPreferenceHelper.UserPreferenceKey.money.name())) {
+                            int money = sharedPreferences.getInt(key, GameManager.getInstance().getUser().getMoney());
+                            GameManager.getInstance().getUser().setMoney(money);
+                            binding.tvMoney.setText(String.valueOf(money));
+                        }
+                    }
+                };
 
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener);
 
         return binding.getRoot();
     }
@@ -238,12 +241,14 @@ public class HomeFragment extends Fragment {
         super.onResume();
         GameManager gameManager = GameManager.getInstance();
         binding.tvMoney.setText(String.valueOf(gameManager.getUser().getMoney()));
+        //Log.d("__walk", String.valueOf(binding.tvMoney.getId()));
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         // 바인딩은 생명주기 이슈 때문에 프래그먼트가 종료되면 널을 넣어줘야 함
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(sharedPreferenceChangeListener);
         binding = null;
     }
 }
