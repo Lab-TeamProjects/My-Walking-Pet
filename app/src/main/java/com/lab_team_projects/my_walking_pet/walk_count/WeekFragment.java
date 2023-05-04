1d53613c44c63c05cfa4075c4949f717b30a75e5
wndudwkd003 <ymail3@naver.com>
2023-05-04 16:33:17 +0900

    버그 수정

--- app/src/main/java/com/lab_team_projects/my_walking_pet/walk_count/WeekFragment.java
+++ app/src/main/java/com/lab_team_projects/my_walking_pet/walk_count/WeekFragment.java
@@ -79,7 +79,7 @@ public class WeekFragment extends Fragment {
                 Calendar nextCal = Calendar.getInstance();
                 nextCal.setTime(nextDate);
                 // 다음 데이터에서 하루를 빼고 비교 데이터로 만든다
-                //nextCal.add(Calendar.DATE, -1);
+                nextCal.add(Calendar.DATE, -1);
 
                 Calendar currentCal = Calendar.getInstance();
 
@@ -101,7 +101,7 @@ public class WeekFragment extends Fragment {
 
                         // 새로운 데이터를 만들어준다
                         String newDate = String.format(Locale.getDefault()
-                                , "%d-%d-%d 00:00:00"
+                                , "%04d-%02d-%02d 00:00:00"
                                 , currentCal.get(Calendar.YEAR)
                                 , currentCal.get(Calendar.MONTH) + 1
                                 , currentCal.get(Calendar.DATE));
