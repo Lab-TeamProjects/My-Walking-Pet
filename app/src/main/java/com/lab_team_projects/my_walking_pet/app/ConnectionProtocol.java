dd9943e42f14a534c3971993deb0ba9ae861f009
HAEDAL-IRLAB <jyh4282002@naver.com>
2023-05-20 15:36:55 +0900

    통신규약 추가

--- app/src/main/java/com/lab_team_projects/my_walking_pet/app/ConnectionProtocol.java
+++ app/src/main/java/com/lab_team_projects/my_walking_pet/app/ConnectionProtocol.java
@@ -12,7 +12,15 @@ public class ConnectionProtocol {
 
     public static final String INVALID_IMAGE_DATA = "D01"; // 잘못된 이미지 파일
     public static final String UNCORRECTABLE_DATA = "D02"; // 수정할 수 없는 데이터
+    public static final String INVALUD_DATE = "D03"; // 유효하지 않은 날짜
+    public static final String INVALUD_DATE_RANGE = "D04"; // 잘못된 날짜 범위
+    public static final String NOT_USER = "D05"; // 없는 유저
+    public static final String NOT_FOUND_PROFILE = "D06"; // 프로필 사진 없음
+    public static final String MYSELF_AN_ETERNAL_FRIEND = "D07"; // 자기 자신 친구추가
 
+    public static final String NOT_MY_PET = "P01"; // 자기 자신의 동물이 아님
+
+    public static final String FAIL = "F00"; // 걍 실패
 
     // 요청 url
     public static final String SIGN_UP = "sign-up";
@@ -26,8 +34,6 @@ public class ConnectionProtocol {
     public static final String PROFILE_PHOTO_UPLOAD = "user/profile_photo/upload";
     public static final String PROFILE_PHOTO_VIEW = "user/profile_photo/view";
     public static final String GAME_DATA_VIEW = "user/game/view";
-
-
 
 
     public static final String LOGIN_TEST = "/login-test"; // 로그인 테스트
